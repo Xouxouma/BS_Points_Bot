97ae92dcd3318bebacd62f69f9c40062f86693b4
grosboisma <grosboisma@eisti.eu>
2020-07-04 10:05:40 +0200

    fix save permission

--- save.js
+++ save.js
@@ -62,8 +62,6 @@ function scoresToJson(map)
 }
 
 module.exports = scores => {
-    if (!message.member.roles.find(r => r.name === "Admin"))
-        return message.reply("Only an Admin can give points! You can't fool probot that easily :P")
     console.log("save")
     var data = scoresToJson(scores)
 
