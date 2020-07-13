ade04375359ca59cc7b467dfe0d60c294f10b5e8
grosboisma <grosboisma@eisti.eu>
2020-07-13 11:56:17 +0200

    display rank number

--- commands/getRanking.js
+++ commands/getRanking.js
@@ -5,7 +5,11 @@ module.exports = (ranking) => {
   if (ranking.size == 0)
     return answer +"No member have points on this ranking yet.\n"
   let sortedRanking = new Map([...ranking.entries()].sort((a, b) => parseFloat(b[1]) - parseFloat(a[1])));
-  sortedRanking.forEach(function(val,key) { answer += key+" ("+val+")\n"})
+  var rank = 1
+  sortedRanking.forEach(function(val,key) {
+    answer += rank + "° - " + key + " : "+val+"\n"
+    rank += 1
+  })
 
   return answer
 }
