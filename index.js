5448277cd00dea5e53c207747dba39ab68a99b40
Xouxouma <36069455+Xouxouma@users.noreply.github.com>
2022-11-24 22:21:42 +0100

    test add intents

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var read = require('./read')
 var readRankingLinks = require('./data_persistency/readRankingLinks')
 var readTitles = require('./data_persistency/readTitles')
 const data_file = './db/scores_data.json'
-const Discord = require('discord.js')
+const {Client, Intents} = require('discord.js')
 const fs = require('fs')
-const client = new Discord.Client()
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+
 const User = require('./model/User.js')
 
 client.login(process.env.BOT_TOKEN)
